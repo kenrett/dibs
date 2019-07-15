c0bb71152237ffa3dd00ab8a461775277c4eeb78
Ken Rettberg <kenrettberg@gmail.com>
2019-07-15 13:39:18 -0700

    eager load /lib as well

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Dibs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Settings in config/environments/* take precedence over those specified here.
